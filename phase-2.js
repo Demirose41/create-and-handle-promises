50d27968b138d9f846be59c9c25be3ffee56ffda
demirose41 <demirose41@gmail.com>
2024-11-09 16:39:52 -0800

    2: completed phase 2

--- phase-2.js
+++ phase-2.js
@@ -1,24 +1,47 @@
 function stretch(timeLeft) {
   // refactor your code from phase 1
-  // Your code here
+  return new Promise((resolve, reject) => {
+    if( timeLeft < 1000) return reject('stretch')
+    setTimeout(() => {
+      console.log('done stretching');
+      resolve(timeLeft - 1000)
+    }
+    , 1000);
+  })
+  // .catch( reason => console.error("Not enough time to stetch!", reason ));
 }
 
 
 function runOnTreadmill(timeLeft) {
-  // refactor your code from phase 1
-  // Your code here
+  return new Promise((resolve, reject) => {
+    if(timeLeft < 500) return reject('run on treadmill')
+      setTimeout(() => {
+        console.log('done running')
+        resolve(timeLeft - 500)
+      }, 500)
+  })
+  // .catch( reason => console.error("not enough time to run on treadmill!", reason));
 }
 
 
 function liftWeights(timeLeft) {
-  // refactor your code from phase 1
-  // Your code here
+  return new Promise((resolve, reject) => {
+    if(timeLeft < 2000) return reject('lift weights');
+    setTimeout(() => {
+      console.log('done lifting');
+      resolve(timeLeft - 2000)
+    }, 2000)
+  })
+  // .catch( reason => console.error("not enough time to lift weights"))
 }
 
 
 function workout(totalTime) {
-  // refactor your code from phase 1
-  // Your code here
+  stretch(totalTime)
+  .then(timeLeft => runOnTreadmill(timeLeft))
+  .then(timeLeft => liftWeights(timeLeft))
+  .then(timeLeft => console.log(`done working out wiht  ${timeLeft/1000} seconds left`))
+  .catch( reason  => console.error('Error: your dont have enough time to ', reason));
 }
 
 /* ============================ TEST YOUR CODE ============================
@@ -46,7 +69,7 @@ Comment in each invocation of your workout function below and run the file
     // Error:  you dont have enough time to lift weights
 
 
-// workout(4000);
+workout(4000);
   // should print out the following:
   //   done stretching
   //   done running on treadmill
